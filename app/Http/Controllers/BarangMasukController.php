eae729d50838f58e791929145a030a270c5385c7
irvan <irvanirawanit@gmail.com>
2020-12-09 23:53:13 +0700

    Get Post Barang Masuk

--- app/Http/Controllers/BarangMasukController.php
+++ app/Http/Controllers/BarangMasukController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use App\Models\BarangMasuk;
 
 class BarangMasukController extends Controller
 {
@@ -15,11 +16,22 @@ class BarangMasukController extends Controller
 
     public function create(Request $request)
     {
-        $data = DB::table('BarangMasuk')->insert([
+        // $data = DB::table('BarangMasuk')->insert([
+        //     "NoBarang"  =>$request->NoBarang,
+        //     "NamaBarang"=>$request->NamaBarang,
+        //     "Jumlah"    =>$request->Jumlah
+        // ]);
+        $data = BarangMasuk::create([
             "NoBarang"  =>$request->NoBarang,
             "NamaBarang"=>$request->NamaBarang,
             "Jumlah"    =>$request->Jumlah
         ]);
+        return response()->json($data);
+    }
+
+    public function show(Request $request,$id)
+    {
+        $data = DB::table('BarangMasuk')->where('BarangMasukId','=',$id)->first();
         return response()->json($data);
     }
 }
--- app/Models/BarangMasuk.php
+++ app/Models/BarangMasuk.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class BarangMasuk extends Model
 {
-    //
+    protected $table = 'BarangMasuk';
+    protected $primaryKey = 'BarangMasukId';
+    protected $fillable = ['NoBarang','NamaBarang','Jumlah','created_by','updated_by','pemilik'];
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
 }
