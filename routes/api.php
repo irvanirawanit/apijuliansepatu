3eb685f76d44278e4307863f0dfb3972c6a30e1f
irvan <irvanirawanit@gmail.com>
2020-12-10 10:48:01 +0700

    cors

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
